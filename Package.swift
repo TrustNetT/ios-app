8e848870976c1d63aff872fb0bb0f26015b0156d
jcgarcia <jcgarcia@example.com>
2026-02-08 21:01:02 +0000

    Feb 8 2026: Add macOS support to Package.swift for local compilation

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "TrustNetApp",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v11)
     ],
     products: [
         .library(name: "TrustNetCore", targets: ["TrustNetCore"]),
