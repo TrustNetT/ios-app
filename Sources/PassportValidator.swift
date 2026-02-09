eb9a5b00259da655165e0bdad4a852d0acdc2acd
jcgarcia <jcgarcia@example.com>
2026-02-09 12:17:50 +0000

    Feb 9 2026: Fix double-hashing bug in signature validation

--- Sources/PassportValidator.swift
+++ Sources/PassportValidator.swift
@@ -10,9 +10,8 @@ public class PassportValidator {
         signature: Data,
         publicKey: Data
     ) throws -> Bool {
-        let digest = SHA256.hash(data: documentData)
         let key = try P256.Signing.PublicKey(rawRepresentation: publicKey)
         let sig = try P256.Signing.ECDSASignature(rawRepresentation: signature)
-        return key.isValidSignature(sig, for: Data(digest))
+        return key.isValidSignature(sig, for: documentData)
     }
 }
